41b0dfa19539149328323b5b696b881a6d65be71
Wan Ching <wanching_97@hotmail.com>
2017-07-28 01:55:23 +0800

    half of DbQueries class

--- app/src/main/java/com/example/android/eventcountdown/EventDbHelper.java
+++ app/src/main/java/com/example/android/eventcountdown/EventDbHelper.java
@@ -1,5 +1,6 @@
 package com.example.android.eventcountdown;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -15,15 +16,21 @@ public class EventDbHelper extends SQLiteOpenHelper {
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE" + EventContract.EventEntry.TABLE_NAME + " (" +
                     EventContract.EventEntry._ID + " INTEGER PRIMARY KEY," +
-                    EventContract.EventEntry.COLUMN_NAME_TITLE + ""
+                    EventContract.EventEntry.COLUMN_NAME_TITLE + " TEXT," +
+                    EventContract.EventEntry.COLUMN_NAME_DESCRIPTION + " TEXT," +
+                    EventContract.EventEntry.COLUMN_NAME_DATE + " INTEGER," +
+                    EventContract.EventEntry.COLUMN_NAME_NOTIFY + " INTEGER)";
+
+    public EventDbHelper (Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
     }
 }
---
+++ app/src/main/java/com/example/android/eventcountdown/EventDbQueries.java
@@ -0,0 +1,55 @@
+package com.example.android.eventcountdown;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by WanChing on 28/7/2017.
+ */
+
+public class EventDbQueries {
+
+    private EventDbHelper helper;
+
+    public EventDbQueries (EventDbHelper helper){
+        this.helper = helper;
+    }
+
+    public Cursor read (String[] columns, String selection, String[] selectionArgs, String groupby, String having, String orderBy){
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        return db.query(
+                EventContract.EventEntry.TABLE_NAME,
+                columns,
+                selection,
+                selectionArgs,
+                groupby,
+                having,
+                orderBy
+        );
+    }
+
+    public long insert (Event event){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(EventContract.EventEntry.COLUMN_NAME_TITLE, event.getTitle());
+        values.put(EventContract.EventEntry.COLUMN_NAME_DESCRIPTION, event.getDescription());
+        //values.put(EventContract.EventEntry.COLUMN_NAME_DATE, event.getDate());
+
+        long id = db.insert(EventContract.EventEntry.TABLE_NAME, null, values);
+        event.setId(id);
+
+        return id;
+    }
+
+    public int update(Event event){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(EventContract.EventEntry.COLUMN_NAME_TITLE, event.getTitle());
+        values.put(EventContract.EventEntry.COLUMN_NAME_DESCRIPTION, event.getDescription());
+
+        String selection = EventContract.EventEntry._ID + " = ?";
+        String [] selectionArgs = {Long.toString(event.getId())};
+    }
+}
